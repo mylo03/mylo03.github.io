42f49cab5e222b881342098d558d67e08fdf3f25
mylo03 <93289716+mylo03@users.noreply.github.com>
2025-06-12 14:56:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // If using a custom domain or user/organization site (e.g., mylo03.github.io)
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
